263e0eb0e95c721a1e4bd0b65402b59670a1d79f
Sahil Verma <domainsahil007@gmail.com>
2022-08-09 22:50:20 +0530

    hero images changed

--- client/components/Hero.jsx
+++ client/components/Hero.jsx
@@ -27,10 +27,7 @@ const Hero = () => {
           </Link>
         </div>
         <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-          <img
-            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png"
-            alt="mockup"
-          />
+          <img src="/images/hero-image.png" alt="mockup" />
         </div>
       </div>
     </section>
